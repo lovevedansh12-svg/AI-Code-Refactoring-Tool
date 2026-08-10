0a39df08f3404223c317dc032e2e3cb739a156e4
lovevedansh12-svg <lovevedansh12@gmail.com>
2026-08-10 19:08:56 +0530

    Implement processData function in sample.cpp
    Added a function to process data with multiple parameters and print results based on conditions.

---
+++ test/sample.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+void processData(int a, int b, int c, int d, int e, int f)
+{
+    if (a > 0)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            if (b > 0)
+            {
+                while (c > 0)
+                {
+                    if (d > 0)
+                    {
+                        cout << a + b + c + d;
+                    }
+                }
+            }
+        }
+    }
+
+    int x1 = 1;
+    int x2 = 2;
+    int x3 = 3;
+    int x4 = 4;
+    int x5 = 5;
+    int x6 = 6;
+    int x7 = 7;
+    int x8 = 8;
+    int x9 = 9;
+    int x10 = 10;
+    int x11 = 11;
+    int x12 = 12;
+    int x13 = 13;
+    int x14 = 14;
+    int x15 = 15;
+    int x16 = 16;
+    int x17 = 17;
+    int x18 = 18;
+    int x19 = 19;
+    int x20 = 20;
+    int x21 = 21;
+    int x22 = 22;
+    int x23 = 23;
+    int x24 = 24;
+    int x25 = 25;
+    int x26 = 26;
+    int x27 = 27;
+    int x28 = 28;
+    int x29 = 29;
+    int x30 = 30;
+    int x31 = 31;
+}
