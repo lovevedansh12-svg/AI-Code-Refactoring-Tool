b7159d097ce017e4a79dd5f708aa861eee67f3d4
lovevedansh12-svg <lovevedansh12@gmail.com>
2026-08-12 21:58:37 +0530

    Add test for RefactoringEngine with C++ code analysis

---
+++ ai-engine/test_refactor.py
@@ -0,0 +1,47 @@
+from refactor import RefactoringEngine
+from model import MockModel
+
+
+code = """
+void calculate(int a, int b, int c, int d, int e, int f)
+{
+    if (a > 0)
+    {
+        if (b > 0)
+        {
+            if (c > 0)
+            {
+                cout << a + b + c;
+            }
+        }
+    }
+}
+"""
+
+
+analysis_report = {
+    "language": "cpp",
+    "total_issues": 2,
+    "issues": [
+        {
+            "type": "deep_nesting",
+            "severity": "high"
+        },
+        {
+            "type": "too_many_parameters",
+            "severity": "medium"
+        }
+    ]
+}
+
+
+model = MockModel()
+
+engine = RefactoringEngine(model)
+
+result = engine.refactor(
+    code,
+    analysis_report
+)
+
+print(result)
